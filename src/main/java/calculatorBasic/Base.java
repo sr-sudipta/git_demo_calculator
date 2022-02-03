e7c6acd16203f9e769448f381edcc4365d142561
sr-sudipta <sudiptaroy.sr7@gmail.com>
2022-02-03 15:07:28 +0530

    Added a Base Class

---
+++ src/main/java/calculatorBasic/Base.java
@@ -0,0 +1,15 @@
+package calculatorBasic;
+
+import java.util.*;
+
+public class Base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choices: " + "\n1.Sum \n2.Subtraction \n3.Multiplication \n4.Division");
+
+	}
+
+}
