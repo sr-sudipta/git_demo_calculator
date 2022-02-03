b241bee6465b8d9c70ef10122e0af224aa2b4309
sr-sudipta <sudiptaroy.sr7@gmail.com>
2022-02-03 15:16:03 +0530

    Added algebric_subtraction class

---
+++ src/main/java/calSub/AlgebricSub.java
@@ -0,0 +1,13 @@
+package calSub;
+
+public class AlgebricSub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5;
+		int num2 = 10;
+		int result = num1 - num2;
+		System.out.println("Subtraction: " + result);
+	}
+
+}
