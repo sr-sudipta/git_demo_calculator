7b743709d85e0480040a196cb9c8939ad203788d
sr-sudipta <sudiptaroy.sr7@gmail.com>
2022-02-03 14:52:23 +0530

    Initial Commit

---
+++ src/main/java/calculatorBasic/HelloCalculator.java
@@ -0,0 +1,17 @@
+package calculatorBasic;
+
+public class HelloCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Calculator!!");
+		/* Step1: Select project
+		 * Right click
+		 * choose team
+		 * share project
+		 * check use or create repo...
+		 * finish
+		 */
+	}
+
+}
