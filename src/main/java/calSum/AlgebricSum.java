e9f546abd3c3cd1a4291d9484fac31e17da558c3
sr-sudipta <sudiptaroy.sr7@gmail.com>
2022-02-03 15:01:53 +0530

    Added albericsum class

---
+++ src/main/java/calSum/AlgebricSum.java
@@ -0,0 +1,15 @@
+package calSum;
+
+import java.util.*;
+
+public class AlgebricSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5;
+		int num2 = 10;
+		int result = num1 + num2;
+		System.out.println("Summation: "+result);
+	}
+
+}
